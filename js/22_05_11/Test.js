8efc6ee02107ee0b927ce4376648fa7e7b714d02
limes22 <howdi2002@naver.com>
2022-05-11 10:13:23 +0000

    4

--- js/22_05_11/Test.js
+++ js/22_05_11/Test.js
@@ -1,16 +1,22 @@
-var n = 3;
+var showSum = function(){
+  var num1 = document.getElementsByTagName("input")[0].value;
+  var result = parseInt(num1)
+  document.getElementsByTagName("input")[0].value=result;
 
-document.write('★'.repeat(n)+'<br>')
+  var n = result;
+  document.write('★'.repeat(n)+'<br>')
 
- for(let j=1; j<=n; j=j+2) {
-   document.write('★'.repeat((-j+n)*0.5)+'○'.repeat(j)+'★'.repeat((-j+n)*0.5)+'<br>')
- }
- 
- for(let j=n-2; j>=1; j=j-2) {
-  document.write('★'.repeat((-j+n)*0.5)+'○'.repeat(j)+'★'.repeat((-j+n)*0.5)+'<br>')
+  for(let j=1; j<=n; j=j+2) {
+    document.write('★'.repeat((-j+n)*0.5)+'○'.repeat(j)+'★'.repeat((-j+n)*0.5)+'<br>')
+  }
+  
+  for(let j=n-2; j>=1; j=j-2) {
+    document.write('★'.repeat((-j+n)*0.5)+'○'.repeat(j)+'★'.repeat((-j+n)*0.5)+'<br>')
+  }
+
+  document.write('★'.repeat(n)+'<br>')
+
 }
-
-document.write('★'.repeat(n)+'<br>')
 
 // for(let j=3; j>=1; j=j-2) {
 //   document.write('★'.repeat(-0.5*j+5.5)+'○'.repeat(j)+'★'.repeat(-0.5*j+5.5)+'<br>')
