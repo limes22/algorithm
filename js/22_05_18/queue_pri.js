efd97263d1b1a751e90744c63cf821bedb4f3dc8
박재호 <inno_pjh@innogrid.com>
2022-05-20 20:00:09 +0900

    Update Code

--- js/22_05_18/queue_pri.js
+++ js/22_05_18/queue_pri.js
@@ -1,27 +1,28 @@
-var queue
-var front = 0;
-var back = 0;
+let queue
+let front = 0;
+let back = 0;
 
 const queueInit = (n) => {
- queue = new Array(n);
- front = 0;
- back = 0;
- console.log('length:', queue.length)
+  queue = new Array(n);
+  front = 0;
+  back = 0;
+  console.log('length:', queue.length)
 }
 
-const enqueue = (inputNumber, priority = 'unknown') => {
+const enqueue = (inputNumber, priority = -1) => {
   if (queue[front] !== undefined) {
     console.log('queue is full')
     return;
   }
-  queue[front] = {inputNumber, priority};
+  queue[front] = { inputNumber, priority };
+  bubbleSort();
   const enqueueData = queue[front]
   front += 1;
   if (front === queue.length){
     front = 0;
   }
- // if (queue.priority === 'unknown') {
- // queue[front] = queue.splice(front +1, 1, {inputNumber, priority}) }
+  // if (queue.priority === 'unknown') {
+  // queue[front] = queue.splice(front +1, 1, {inputNumber, priority}) }
   console.log('enqueue data: ', enqueueData);
 }
 
@@ -37,20 +38,23 @@ const dequeue = () => {
     back = 0;
   }
   console.log('dequeue data: ', dequeueData);
-  
+
 }
 
 
-const showAll = () => { 
-    for (i =0; i < queue.length; i ++) {
-      console.log('queue: ', queue[i])
-    } 
+const showAll = () => {
+  for (i =0; i < queue.length; i ++) {
+    console.log('queue: ', queue[i])
+  }
 }
 
 const bubbleSort = () => {
-  for (i = 0; i < queue.length; i++) {
-    for (j = i+1; j < queue.length; j++) {
-      if (queue[i]['priority'] > queue[j]['priority']) {
+  for (let i = 0; i < queue.length; i++) {
+    for (let j = i+1; j < queue.length; j++) {
+      if (!queue[j]) {
+        continue;
+      }
+      if (queue[i].priority > queue[j].priority) {
         const temp = queue[i];
         queue[i] = queue[j];
         queue[j] = temp;
@@ -61,17 +65,10 @@ const bubbleSort = () => {
 
 
 queueInit(4)
-enqueue(6,5);
+enqueue(6, 5);
 enqueue(5, 4);
 enqueue(4, 3);
-enqueue(3, 2);
-dequeue();
 enqueue(2, 1);
-bubbleSort ();
-showAll();
-dequeue();
-dequeue();
-dequeue();
 dequeue();
 dequeue();
 showAll();
