0b4240fa8c9f2ee356491af77f8d32439880d611
박재호 <inno_pjh@innogrid.com>
2022-05-20 20:42:31 +0900

    bugfix 1

--- js/22_05_18/queue_pri.js
+++ js/22_05_18/queue_pri.js
@@ -1,4 +1,4 @@
-let queue
+let queue;
 let front = 0;
 let back = 0;
 
@@ -6,52 +6,51 @@ const queueInit = (n) => {
   queue = new Array(n);
   front = 0;
   back = 0;
-  console.log('length:', queue.length)
-}
+  console.log("length:", queue.length);
+};
 
 const enqueue = (inputNumber, priority = -1) => {
   if (queue[front] !== undefined) {
-    console.log('queue is full')
+    console.log("queue is full");
     return;
   }
   queue[front] = { inputNumber, priority };
   bubbleSort();
-  const enqueueData = queue[front]
+  const enqueueData = queue[front];
   front += 1;
-  if (front === queue.length){
+  if (front === queue.length) {
     front = 0;
   }
   // if (queue.priority === 'unknown') {
   // queue[front] = queue.splice(front +1, 1, {inputNumber, priority}) }
-  console.log('enqueue data: ', enqueueData);
-}
+  console.log("enqueue data: ", enqueueData);
+};
 
 const dequeue = () => {
-  if (queue[back] === undefined){
-    console.log('queue is empty')
+  if (queue[back] === undefined) {
+    console.log("queue is empty");
     return;
   }
-  const dequeueData = queue[back]
+  const dequeueData = queue[back];
   queue[back] = undefined;
-  back += 1
+  back += 1;
   if (back === queue.length) {
     back = 0;
   }
-  console.log('dequeue data: ', dequeueData);
-
-}
-
+  console.log("dequeue data: ", dequeueData);
+};
 
 const showAll = () => {
-  for (i =0; i < queue.length; i ++) {
-    console.log('queue: ', queue[i])
+  for (let i = 0; i < queue.length; i++) {
+    console.log("queue", [i], " : ", queue[i]);
   }
-}
+};
 
 const bubbleSort = () => {
   for (let i = 0; i < queue.length; i++) {
-    for (let j = i+1; j < queue.length; j++) {
-      if (!queue[j]) {
+    for (let j = i + 1; j < queue.length; j++) {
+      // queue[i]
+      if (!queue[i] || !queue[j]) {
         continue;
       }
       if (queue[i].priority > queue[j].priority) {
@@ -61,14 +60,14 @@ const bubbleSort = () => {
       }
     }
   }
-}
+};
 
-
-queueInit(4)
-enqueue(6, 5);
-enqueue(5, 4);
-enqueue(4, 3);
-enqueue(2, 1);
+queueInit(3);
+enqueue(1, 5);
+enqueue(1, 3);
+enqueue(1, 1);
 dequeue();
+dequeue();
+enqueue(1, 1);
 dequeue();
 showAll();
