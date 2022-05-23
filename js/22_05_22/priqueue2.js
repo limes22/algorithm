0063d99773a1714a7ee705d870ebbc1c5ccbd2d0
limes22 <howdi2002@naver.com>
2022-05-23 08:02:27 +0000

    priqueue03

--- js/22_05_22/priqueue2.js
+++ js/22_05_22/priqueue2.js
@@ -1,62 +1,79 @@
 let queue;
+let back = 0;
+
 const queueInit = (n) => {
-  queue = new Array(n)
+  queue = new Array(n);
+  back = 0;
 }
 
 const enqueue = (inputNumber, priority = -1) => {
-  for(let i=0; i<queue.length; i++) {
-    if(!queue[i]) {
-      queue[i] = {inputNumber, priority}
-      const enqueue = queue[i]
-      console.log(`enqueue:`, enqueue)
-      return;
+  for (let i=0; i<queue.length; i++) {
+    if (!queue[i]) {
+    queue[i] = {inputNumber, priority}
+    const enqueuedata = queue[i]
+    console.log(`enqueuedata:`, enqueuedata)
+    return;
     }
   }
-  console.log("queue is full")
+  console.log('queue is full')
 }
 
 const dequeue = () => {
-  let priority;
-  let priIdx;
-  for(let i=0; i<queue.length; i++) {
+  var priority;
+  var priorityIdx;
+  for (let i = 0; i < queue.length; i++) {
     if (!queue[i]) {
       continue;
     }
     if (!priority) {
-      priority = queue[i].priority
-      priIdx = i;
+      priority = queue[i].priority;
+      priorityIdx = i;
       continue;
     }
     if (queue[i].priority <= priority) {
       priority = queue[i].priority;
-      priIdx = i;
+      priorityIdx = i;
     }
-    if (queue[priIdx] === undefined) {
-      console.log("queue is empty");
-      return;
-    }
-    const dequeue = queue[priIdx]
-    console.log(`dequue: `,dequeue)
-    queue[priIdx] = undefined;
   }
-}
+  const dequeue = queue[priorityIdx]
+  console.log(`dequue: `,dequeue)
+  queue[priorityIdx] = undefined;
+  if (!priorityIdx) {
+    console.log("queue is empty")
+    return;
+  }
+};
+
+
 
 const showAll = () => {
-  for(let i=0; i<queue.length; i++) {
+  for (let i=0; i<queue.length; i++) {
     console.log(`queue : `, queue[i])
   }
 }
 
+const bubbleSort = () => {
+  for (let i = 0; i < queue.length; i++) {
+    for (let j = i + 1; j < queue.length; j++) {
+      // queue[i]
+      if (!queue[i] || !queue[j]) {
+        continue;
+      }
+      if (queue[i].priority > queue[j].priority) {
+        const temp = queue[i];
+        queue[i] = queue[j];
+        queue[j] = temp;
+      }
+    }
+  }
+}
+
+
 queueInit(3)
-enqueue(4,2)
-enqueue(5,3)
-enqueue(6,4)
-showAll()
-dequeue()
-dequeue()
-showAll()
+enqueue(5,4)
+enqueue(6,5)
 enqueue(7,3)
-enqueue(8,2)
+dequeue()
 dequeue()
 dequeue()
 dequeue()
