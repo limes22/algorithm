cac8d480f269cc8fc5893d1fa41a6da229ce842f
limes22 <howdi2002@naver.com>
2022-06-12 00:03:57 +0000

    priqueue3

--- js/22_05_22/priqueue2.js
+++ js/22_05_22/priqueue2.js
@@ -2,64 +2,49 @@ let queue;
 
 const queueInit = (n) => {
   queue = new Array(n);
-  back = 0;
 }
 
 const enqueue = (inputNumber, priority = -1) => {
-  for (let i=0; i<queue.length; i++) {
-    if (!queue[i]) {
-    queue[i] = {inputNumber, priority}
-    const enqueuedata = queue[i]
-    console.log(`enqueuedata:`, enqueuedata)
-    return;
-    }
+  for (let i = 0; i < queue.length; i++){
+    if (!queue[i]){
+      queue[i] = {inputNumber, priority};
+      return;
   }
-  console.log('queue is full')
+}
 }
 
 const dequeue = () => {
   let priority;
-  let priorityIdx;
-  for (let i = 0; i < queue.length; i++) {
-    if (!queue[i]) {
+  let priIdx;
+  for (let i = 0; i < queue.length; i++){
+    if (!queue[i]){
       continue;
     }
     if (!priority) {
-      priority = queue[i].priority;
-      priorityIdx = i;
-      console.log(`priorityIdx(1)`, priorityIdx)
+      priority = queue[i].priority
+      priIdx = i
       continue;
     }
-    if (queue[i].priority <= priority) {
+    if (priority > queue[i].priority){
       priority = queue[i].priority;
-      priorityIdx = i;
-      console.log(`priorityIdx(2)`, priorityIdx)
+      priIdx = i
     }
   }
-  console.log(`priorityIdx(3)`, priorityIdx)
-  const dequeue = queue[priorityIdx]
-  console.log(`dequue: `,dequeue)
-    if (queue[priorityIdx] === undefined) {
-    console.log(`queue is empty`)
+  if (!queue[priIdx])
+  {
+    console.log('queue is empty')
+    return;
   }
-  queue[priorityIdx] = undefined;
-};
-
-
-
-const showAll = () => {
-  for (let i=0; i<queue.length; i++) {
-    console.log(`queue : `, queue[i])
-  }
+  console.log('dequeue data:', queue[priIdx])
+  queue[priIdx] = undefined
 }
 
-
-
 queueInit(3)
-enqueue(5, 2)
 enqueue(6, 3)
-enqueue(8, 1)
+enqueue(7, 2)
+enqueue(9, 4)
 dequeue()
 dequeue()
 dequeue()
-showAll()
+dequeue()
+console.log(queue)
