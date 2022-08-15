c7c96a52f915ceb71285eae0c39983b430de01ee
limes22 <howdi2002@naver.com>
2022-08-15 11:13:23 +0000

    star

--- go/star/star.go
+++ go/star/star.go
@@ -5,15 +5,25 @@ import (
 	"strings"
 )
 
-func main() {
-	PrintTriangle(5)
+func PrintTriangle(num int) string {
+	var star string = "☆"
+	var point string = "○"
+	pnt1 := strings.Repeat(point, num)
+	fmt.Println(pnt1)
+	for i := 1; i <= num; i = i + 2 {
+		pnt2 := strings.Repeat(point, (-i+num)*1/2)
+		str1 := strings.Repeat(star, i)
+		fmt.Println(pnt2, str1, pnt2)
+	}
+	for j := num - 2; j >= 1; j = j - 2 {
+		pnt3 := strings.Repeat(point, (-j+num)*1/2)
+		str2 := strings.Repeat(star, j)
+		fmt.Println(pnt3, str2, pnt3)
+	}
+	fmt.Println(pnt1)
+	return ""
 }
 
-func PrintTriangle(num int) string {
-	var star string = "*"
-	var point string = "."
-	for i := 1; i <= num; i += 2 {
-		fmt.Printf("%6s"+"%6s\n", strings.Repeat(star, i), strings.Repeat(point, i))
-	}
-	return ""
+func main() {
+	fmt.Println(PrintTriangle(3))
 }
