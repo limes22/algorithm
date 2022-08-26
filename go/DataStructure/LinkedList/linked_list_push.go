2733e05cf026189cb7fe58d58c3e61e0e6309473
limes22 <howdi2002@naver.com>
2022-08-26 19:12:08 +0900

    linked_list

--- go/DataStructure/LinkedList/linked_list_push.go
+++ go/DataStructure/LinkedList/linked_list_push.go
@@ -1,8 +1,6 @@
 package LinkedList
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type Node struct {
 	data int
@@ -13,54 +11,50 @@ type Main struct {
 	head *Node
 }
 
-func (n *Node) NodeInit(d int) Node {
+func (n *Node) NodeInit(d int) *Node {
 	node := Node{
 		data: d,
 		next: nil,
 	}
-	return node
+	return &node
 }
 
-func (n *Node) Push(new_data int) *Node {
-	var m Main
-	n.data = new_data
+func (m *Main) Push(new_data int) {
 	new_node := new(Node)
+	new_node.data = new_data
 	new_node.next = m.head
 	m.head = new_node
-	return m.head
 }
 
-func (n *Node) InsertAfter(prev_node *Node, new_data int) *Node {
+func (m *Main) InsertAfter(prev_node *Node, new_data int) {
 	if prev_node == nil {
 		fmt.Println("The given Previous node canot be null")
-		return nil
+		return
 	}
-	n.data = new_data
 	new_node := new(Node)
+	new_node.data = new_data
 	new_node.next = prev_node.next
 	prev_node.next = new_node
-	return prev_node.next
 }
 
-func (n *Node) Append(new_data int) *Node {
-	var m Main
-	n.data = new_data
+func (m *Main) Append(new_data int) {
 	new_node := new(Node)
+	new_node.data = new_data
 	if m.head == nil {
 		m.head = new(Node)
-		return nil
+		m.head.data = new_data
+		return
 	}
 	new_node.next = nil
 	last := m.head
 	if last.next != nil {
 		last = last.next
+		last.next = new_node
+		return
 	}
-	last.next = new_node
-	return last.next
 }
 
-func (n *Node) PrintList() {
-	var m Main
+func (m *Main) PrintList() {
 	tnode := m.head
 	if tnode != nil {
 		fmt.Println(tnode.data, "")
@@ -68,13 +62,11 @@ func (n *Node) PrintList() {
 	}
 }
 func main() {
-	var n Node
-	llist := new(Main)
-	llist.head = n.Append(6)
-	llist.head = n.Push(7)
-	llist.head = n.Push(1)
-	llist.head = n.Append(4)
-	llist.head = n.InsertAfter(llist.head.next, 8)
-	fmt.Println("Created Linked list is")
-	n.PrintList()
+	var m Main
+	m.Append(8)
+	m.Push(7)
+	m.Push(1)
+	m.Append(4)
+	m.InsertAfter(m.head.next, 8)
+	m.PrintList()
 }
