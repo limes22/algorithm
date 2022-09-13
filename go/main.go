f5e659ac3e2f072e8a313d265f1db4a4babd277b
limes22 <howdi2002@naver.com>
2022-09-13 19:43:53 +0900

    practice 2022-09-13

--- go/main.go
+++ go/main.go
@@ -4,38 +4,35 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
+	"sort"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	input := scanner.Text()
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
 
-	result := 0
-	tmp := 0
-	plusOperator := true
-	for _, s := range input {
-		if i, err := strconv.Atoi(string(s)); err == nil {
-			tmp = tmp*10 + i
-			continue
-		}
+	var n int
+	fmt.Fscanln(reader, &n)
+	var a = make([]int, n)
+	var b = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscanf(reader, "%d ", &a[i])
+		fmt.Println(i)
+	}
+	//fmt.Fscanf(&f)
+	for i := 0; i < n; i++ {
+		fmt.Println("second", i)
+		fmt.Fscanf(reader, "%d ", &b[i])
+	}
+	sort.Ints(a)
+	sort.Slice(b, func(i, j int) bool {
+		return b[i] > b[j]
+	})
 
-		if plusOperator {
-			result += tmp
-		} else {
-			result -= tmp
-		}
-		tmp = 0
-		// 맨 처음 -가 나온 시점. 이후 들어오는 값은 모두 뺀다
-		if plusOperator == true && s == '-' {
-			plusOperator = false
-		}
+	var result int
+	for i := 0; i < n; i++ {
+		result += a[i] * b[i]
 	}
-	if plusOperator {
-		result += tmp
-	} else {
-		result -= tmp
-	}
-	fmt.Print(result)
+	fmt.Fprintln(writer, result)
 }
